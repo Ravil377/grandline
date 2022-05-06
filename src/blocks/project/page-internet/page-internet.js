dc5f04f90c48b26f73e57bc669e2a77827a2844f
Ravil Shamilov <75186385+Ravil377@users.noreply.github.com>
2022-05-06 12:44:59 +0500

    Updates

--- src/blocks/project/page-internet/page-internet.js
+++
@@ -1,8 +0,0 @@
-import { overlayDisabled, sliderCard } from "../popular-tarif/popular-tarif.js";
-
-// Интернет
-let internetSlider;
-
-internetSlider = document.querySelector('.internet__content-js');
-if(internetSlider) swiperInternetSlider = sliderCard('internet');
-overlayDisabled();
--- src/blocks/project/page-telephony/page-telephony.js
+++
@@ -1,8 +0,0 @@
-import { overlayDisabled, sliderCard } from "../popular-tarif/popular-tarif.js";
-
-// Телефония
-let telephonySlider;
-telephonySlider = document.querySelector('.telephony__content-js');
-
-if(telephonySlider) swiperTelephonySlider = sliderCard('telephony');
-overlayDisabled();
